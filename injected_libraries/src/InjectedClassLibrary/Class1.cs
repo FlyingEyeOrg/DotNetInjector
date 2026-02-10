e8a2b6dbf4d81b251971f9bcdf96b888180572f8
UnknowName <13604461032@163.com>
2026-02-10 20:52:41 +0800

    添加类库模板。

---
+++ injected_libraries/src/InjectedClassLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace InjectedClassLibrary
+{
+    public class InjectedInterface
+    {
+        public static int Inject(string args)
+        {
+            //Path.Combine(AppContext.BaseDirectory, "")
+            Assembly.LoadFrom("");
+
+            return 0;
+        }
+    }
+}
