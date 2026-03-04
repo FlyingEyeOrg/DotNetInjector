849efb9f6287d2b1b3ea417a633baf3aec8fa456
UnknowName <13604461032@163.com>
2026-03-04 10:06:13 +0800

    添加测试 demo

---
+++ DotNetInjector/demo/InjectionClassLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectionClassLibrary
+{
+    public class InjectionClass
+    {
+        public static int InjectionMethod(string value)
+        {
+            Console.WriteLine("print value: " + value);
+            Console.WriteLine("方法强制调用成功");
+
+            var asm = Assembly.GetEntryAssembly();
+
+            Console.WriteLine("Entry Assembly: " + asm?.FullName);
+
+            return 0;
+        }
+    }
+}
---
+++ DotNetInjector/demo/InjectionDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Environment.Is64BitProcess)
+            {
+                Console.WriteLine("arc: x64");
+            }
+            else
+            {
+                Console.WriteLine("arc: x86");
+            }
+
+            Console.WriteLine("process id: " + Process.GetCurrentProcess().Id);
+            Console.WriteLine("enter any ke to exit...");
+            Console.ReadKey();
+        }
+    }
+}
