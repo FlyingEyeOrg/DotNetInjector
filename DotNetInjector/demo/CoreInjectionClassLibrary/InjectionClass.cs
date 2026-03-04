be49eb27d9ad2325a4df55a1c874f21fbb750496
UnknowName <13604461032@163.com>
2026-03-04 14:34:43 +0800

    添加 demo 到解决方案。

---
+++ DotNetInjector/demo/CoreInjectionClassLibrary/InjectionClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace CoreInjectionClassLibrary
+{
+    public class InjectionClass
+    {
+        public static int InjectionMethod(string value)
+        {
+            Console.WriteLine("print value: " + value);
+            Console.WriteLine("方法强制调用成功");
+
+            var asm = Assembly.GetEntryAssembly();
+
+            Console.WriteLine("Entry Assembly: " + asm?.FullName);
+
+            return 0;
+        }
+    }
+}
--- DotNetInjector/demo/CoreInjectionDemo/Program.cs
+++ DotNetInjector/demo/CoreInjectionDemo/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
-namespace InjectionDemo
+namespace CoreInjectionDemo
 {
     internal class Program
     {
